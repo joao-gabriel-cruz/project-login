faaf251e8f51ea31ead2eff33fcdf85b8dddc7a8
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-04-19 13:53:36 -0300

    arquivos restantes do ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,39 +15,22 @@ function App() {
 
   return (
     <Router>
-      <div className='flex items-center justify-center flex-col text-gray-500 w-full h-screen '>
-        <header className=''>
-          <h1 className='text-cyan-900 text-3xl' >Bem vindo</h1>
-        </header>
-        <section>
-          <Link className='text-sky-500'  to="/">Home</Link>
-
-        </section>
-
         <Routes>
           <Route path='/cadastro' element={<Cadastro />} />
           <Route path='/login' element={<Login />} />
           <Route path='/' element={<Home />} />
         </Routes>
-      </div>
+      
     </Router>
   );
 }
 
 function Home() {
   return (
-
     <div>
-      <nav className='flex items-center justify-center space-x-80'>
-        <div >
-          <p className='text-cyan-900 text-3xl'>você já é um cliente ******* ?</p>
-          <Link className='text-sky-500 ' to="/login">Login</Link>
-        </div>
-        <div >
-          <p className='text-cyan-900 text-3xl'>venha ser um cliente ****** !</p>
-          <Link className='text-sky-500' to="/cadastro">Cadastro</Link>
-        </div>
-      </nav>
+      <header className='flex justify-center'>
+          <h1 className='text-cyan-900 text-3xl' >Bem vindo</h1>
+      </header>
     </div>
   );
 }
--- src/login.jsx
+++ src/login.jsx
@@ -1,22 +1,37 @@
-function Login(){
+import { useNavigate, Link } from "react-router-dom";
+
+
+
+
+function Login() {
+  let navigate = useNavigate();
+
+  function Home() {
+
+    return navigate("/");
+  }
+
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col">
-    <div className='space-y-5 w-80'>
-      <h1 className='text-2xl mb-6' >Login</h1>
-      <div className=''>
-        <input className='border rounded text-gray-500 h-14 w-full px-4 py-2 ' type="text" placeholder='username' />
-      </div>
-      <div>
-        <input className='border rounded text-gray-500 h-14 w-full px-4 py-2' type="password" placeholder='password' />
-      </div>
-     
-      <div className='bg-blue-600 text-white rounded-md flex justify-center cursor-pointer hover:bg-blue-500 ta'>
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z" /></svg> 
-      <input type="submit" value="Register" className='h-14' />
-      </div>
-      <form action=""></form>
+      <form onSubmit={Home} >
+        <div className='space-y-5 w-80'>
+          <h1 className='text-2xl mb-6' >Login</h1>
+          <div className=''>
+            <input className='border rounded text-gray-500 h-14 w-full px-4 py-2 ' type="text" placeholder='username' />
+          </div>
+          <div>
+            <input className='border rounded text-gray-500 h-14 w-full px-4 py-2' type="password" placeholder='password' />
+          </div>
+          <div>
+            <button className='bg-blue-600 text-white rounded-md flex justify-center cursor-pointer hover:bg-blue-500 w-full'>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-5 my-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 11l3-3m0 0l3 3m-3-3v8m0-13a9 9 0 110 18 9 9 0 010-18z" /></svg>
+              <input type="submit" value="Login" className='h-14' />
+            </button>
+            <Link className='text-sky-500 flex justify-end' to="/cadastro">Cadastro</Link>
+          </div>
+        </div>
+      </form>
     </div>
-  </div>
 
   );
 }
